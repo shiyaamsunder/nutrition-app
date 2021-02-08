bf4837bd622f72224f9766ec1960742d1b490c34
shiyaamsunder <63956643+shiyaamsunder@users.noreply.github.com>
2021-02-08 20:39:12 +0530

    initial commit

---
+++ src/components/Login.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import url from '../config/url'
+
+export default class Login extends Component {
+    constructor(props){
+        super(props)
+
+        this.state={
+            loginSuccess: null
+        }
+        this.user={}
+        this.fields={}
+
+    }
+
+    readInput(event, key){
+        this.user[key]= event.target.value
+        this.fields[key]=event.target
+
+    }
+
+    login(){
+        
+        
+        fetch(url.BASE_URL + url.LOGIN_URL, {
+            method: "POST",
+            body: JSON.stringify(this.user),
+            headers: {"Content-Type": "application/json"}
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            
+        })
+        .catch(err=>console.log(err))
+    }
+    render() {
+        return (
+            <div className="container border rounded p-3" style={{marginTop: "3rem"}} >
+                <h1>Login Page</h1>
+                <div className="container-fluid mt-4 w-75 p-3">
+                {
+                    this.state.loginSuccess ===false ? (
+                        <div className="alert alert-danger">
+                            Wrong username or password
+                        </div>
+                    ): null
+                }
+                    <div className="form-group mt-3">
+                        <input 
+                        type="text" 
+                        placeholder="Email" 
+                        className="form-control"
+                        onChange={(event)=> this.readInput(event, "email")}/>
+                    </div>
+                    <div className="form-group mt-3">
+                        <input 
+                        type="password" 
+                        placeholder="Password" 
+                        className="form-control"
+                        onChange={(event)=> this.readInput(event, "password")}/>
+                    </div>
+                    <div className="d-flex align-items-center justify-content-between p-2  ml-auto">
+                        <button onClick={()=>this.login()} className="btn btn-primary">Login</button>
+                        <div className="d-flex flex-column align-items-center  ">
+                        Don't have an account? <Link to="/register">Register here.</Link>   
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
---
+++ src/config/url.js
@@ -0,0 +1,8 @@
+const url = {
+    BASE_URL: 'http://localhost:8000/',
+    REGISTRATION_URL: 'user/register',
+    LOGIN_URL: 'user/login',
+
+}
+
+export default url
